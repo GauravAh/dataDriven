575fee3eff55989f3dac7add8cbf06c64949e61a
GauravAh <131653274+GauravAh@users.noreply.github.com>
2023-07-02 16:38:38 +0530

    Update SetUpClass.java

--- src/test/java/BasePackage/SetUpClass.java
+++ src/test/java/BasePackage/SetUpClass.java
@@ -3,7 +3,6 @@ package BasePackage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SetUpClass{
